364c304b65435b27602ad270b4672e18075a7430
Soroush Saffari <sor.saffari@gmail.com>
2019-01-09 15:35:14 +0000

    upgrade migration scripts

--- applications/tube-network/tube_network_example/settings.py
+++ applications/tube-network/tube_network_example/settings.py
@@ -18,5 +18,5 @@ routes_path = "{}/{}/routes/".format(os.path.dirname(__file__), download_dir_nam
 timetables_path = "{}/{}/timetables/".format(os.path.dirname(__file__), download_dir_name)
 migration_logs_path = "{}/src/migrations/logs/".format(os.path.dirname(__file__))
 
-uri = 'http://localhost:4567'
+uri = 'localhost:48555'
 keyspace = "tube_example"
