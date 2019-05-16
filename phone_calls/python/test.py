f2217305faf9d15058d30d057ab2638a1f8bbd01
The-Alchemist <kap4020@gmail.com>
2019-05-16 18:05:20 -0400

    Porting to Python3

--- phone_calls/python/test.py
+++ phone_calls/python/test.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from grakn.client import GraknClient
 
 import unittest
@@ -42,41 +44,41 @@ class Test(unittest.TestCase):
 
         with self._session.transaction().read() as transaction:
             first_actual_answer = queries.query_examples[0].get("query_function")("", transaction)
-            first_expected_answer = [u"+370 351 224 5176", u"+54 398 559 0423", u"+62 107 530 7500", u"+63 815 962 6097",
-                                     u"+7 690 597 4443", u"+263 498 495 0617", u"+81 308 988 7153", u"+81 746 154 2598"]
-            self.assertItemsEqual(first_actual_answer, first_expected_answer)
+            first_expected_answer = ["+370 351 224 5176", "+54 398 559 0423", "+62 107 530 7500", "+63 815 962 6097",
+                                     "+7 690 597 4443", "+263 498 495 0617", "+81 308 988 7153", "+81 746 154 2598"]
+            self.assertCountEqual(first_actual_answer, first_expected_answer)
 
             second_actual_answer = queries.query_examples[1].get("query_function")("", transaction)
-            second_expected_answer = [u"+351 272 414 6570", u"+30 419 575 7546", u"+1 254 875 4647", u"+86 892 682 0628",
-                                      u"+33 614 339 0298", u"+351 515 605 7915", u"+86 922 760 0418", u"+63 808 497 1769",
-                                      u"+86 825 153 5518", u"+48 894 777 5173", u"+27 117 258 4149", u"+86 202 257 8619"]
-            self.assertItemsEqual(second_actual_answer, second_expected_answer)
+            second_expected_answer = ["+351 272 414 6570", "+30 419 575 7546", "+1 254 875 4647", "+86 892 682 0628",
+                                      "+33 614 339 0298", "+351 515 605 7915", "+86 922 760 0418", "+63 808 497 1769",
+                                      "+86 825 153 5518", "+48 894 777 5173", "+27 117 258 4149", "+86 202 257 8619"]
+            self.assertCountEqual(second_actual_answer, second_expected_answer)
 
             third_actual_answer = queries.query_examples[2].get("query_function")("", transaction)
-            third_expected_answer = [u"+86 892 682 0628", u"+54 398 559 0423"]
-            self.assertItemsEqual(third_actual_answer, third_expected_answer)
+            third_expected_answer = ["+86 892 682 0628", "+54 398 559 0423"]
+            self.assertCountEqual(third_actual_answer, third_expected_answer)
 
             forth_actual_answer = queries.query_examples[3].get("query_function")("", transaction)
-            forth_expected_answer = [u"+261 860 539 4754", u"+81 308 988 7153", u"+62 107 530 7500", u"+261 860 539 4754",
-                                     u"+81 308 988 7153", u"+62 107 530 7500"]
-            self.assertItemsEqual(forth_actual_answer, forth_expected_answer)
+            forth_expected_answer = ["+261 860 539 4754", "+81 308 988 7153", "+62 107 530 7500", "+261 860 539 4754",
+                                     "+81 308 988 7153", "+62 107 530 7500"]
+            self.assertCountEqual(forth_actual_answer, forth_expected_answer)
 
             fifth_actual_answer = queries.query_examples[4].get("query_function")("", transaction)
             fifth_expected_answer = [1242.7714285714285, 1699.4308943089432]
-            self.assertItemsEqual(fifth_actual_answer, fifth_expected_answer)
+            self.assertCountEqual(fifth_actual_answer, fifth_expected_answer)
 
     def assert_migration_results(self):
         with self._session.transaction().read() as transaction:
-            number_of_people = transaction.query("match $x isa person; get $x; count;").next().number()
+            number_of_people = next(transaction.query("match $x isa person; get $x; count;")).number()
             self.assertEqual(number_of_people, 30)
 
-            number_of_companies = transaction.query("match $x isa company; get $x; count;").next().number()
+            number_of_companies = next(transaction.query("match $x isa company; get $x; count;")).number()
             self.assertEqual(number_of_companies, 1)
 
-            number_of_contracts = transaction.query("match $x isa contract; get $x; count;").next().number()
+            number_of_contracts = next(transaction.query("match $x isa contract; get $x; count;")).number()
             self.assertEqual(number_of_contracts, 10)
 
-            number_of_calls = transaction.query("match $x isa call; get $x; count;").next().number()
+            number_of_calls = next(transaction.query("match $x isa call; get $x; count;")).number()
             self.assertEqual(number_of_calls, 200)
 
     def tearDown(self):
