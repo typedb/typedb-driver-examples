4b246d46c85dab7013114d90bf0973b02d1da370
Max Vorobev <vmax0770@gmail.com>
2020-03-01 21:20:18 +0300

    Checkstyle: update copyright header to 2020

--- tube_network/src/app.py
+++ tube_network/src/app.py
@@ -1,4 +1,4 @@
-# Copyright 2019 Grakn Labs Ltd
+# Copyright 2020 Grakn Labs
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
