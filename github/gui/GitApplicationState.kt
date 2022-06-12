762b4883309e3e66b0866fcf85e328357ad78fa7
jamesreprise <51956016+jamesreprise@users.noreply.github.com>
2022-06-12 22:33:25 +0100

    feat: beginnings of github example
    ideal data format would be a single file with repo info, and files, commits, users arrays

---
+++ github/gui/GitApplicationState.kt
@@ -0,0 +1,46 @@
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.window.Notification
+import androidx.compose.ui.window.TrayState
+import common.Settings
+import window.GitWindowState
+
+@Composable
+fun rememberApplicationState() = remember {
+    GitApplicationState().apply {
+        newWindow()
+    }
+}
+
+class GitApplicationState {
+    val settings = Settings()
+    val tray = TrayState()
+    val dbConn = TypeDB
+
+    private val _windows = mutableStateListOf<GitWindowState>()
+    val windows: List<GitWindowState> get() = _windows
+
+    fun newWindow() {
+        _windows.add(
+            GitWindowState(
+                application = this,
+                path = null,
+                exit = _windows::remove
+            )
+        )
+    }
+
+    fun sendNotification(notification: Notification) {
+        tray.sendNotification(notification)
+    }
+
+    suspend fun exit() {
+        val windowsCopy = windows.reversed()
+        for (window in windowsCopy) {
+            if (!window.exit()) {
+                break
+            }
+        }
+    }
+}
---
+++ github/gui/main.kt
@@ -0,0 +1,10 @@
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.window.application
+import common.LocalAppResources
+import common.rememberAppResources
+
+fun main() = application {
+    CompositionLocalProvider(LocalAppResources provides rememberAppResources()) {
+        GitApplication(rememberApplicationState())
+    }
+}
